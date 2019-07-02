659922b2ad3a2205c3d87b86ee3157da54a91d03
Pietro Colombo <pietrocolombo@me.com>
2019-07-02 10:26:40 +0200

    aggiunte immagini scaricate a mano e modifica script per il download

--- gui/download_image.py
+++ gui/download_image.py
@@ -11,7 +11,7 @@ def makeSoup(url):
     #This will load the webpage for the given url
     #page = requests.get(url) 
     #this BeautifulSoup below will parse the html file
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request(url, headers={'User-Agent': 'Firefox/12.0'})
     page=urlopen(req).read()
 
     soup = BeautifulSoup(page, "html5lib")
@@ -19,36 +19,39 @@ def makeSoup(url):
 
 
 #This function will be called every new keyword line is encountered and will start scraping the amazon web page of the search result according to the text mention in the keywords text file
-def perfromScraping(urlReceived, product_id):
+def perfromScraping(product_id):
+    url = f'https://www.amazon.com/dp/{product_id}'
     breaki = 1
     breakPointNumber = 1
-    url = urlReceived
+    #url = urlReceived
     soup = makeSoup(url)
     print('entrati')
     i = 1
     title = soup.findAll(id = 'productTitle')
-    print(title)
+    #print(title)
     title_str = ''
-    #if title:
-    #    print('entrato nel primo')
-    #    tag = re.findall('>[^<]*<', str(title))
-    #    title_str = tag[0]
-    #    title_str = title_str.replace('\n', '')
-    #    title_str = title_str[1:-1]
-    #    title_str = title_str.strip()
-    #if not soup.findAll('img', alt = title_str):
-    #    print('entrato nel secondo')
-    #    title = soup.findAll(meta = 'title')
-    #    print(title)
-    #    title_str = title.get('title')
+    if title:
+        print('entrato nel primo')
+        tag = re.findall('>[^<]*<', str(title))
+        title_str = tag[0]
+        title_str = title_str.replace('\n', '')
+        title_str = title_str[1:-1]
+        title_str = title_str.strip()
+    if not soup.findAll('img', alt = title_str):
+        print('entrato nel secondo')
+        title = soup.findAll(meta = 'title')
+        print(title)
+        title_str = title.get('title')
+    if not soup.findAll('img', alt = title_str):
+        titolo = soup.findAll('title')
+        print(titolo)
+        title_arr = re.split(':',str(titolo[0]))
+        print(title_arr)
+        title_str = title_arr[1]
+        if(len(title_arr) == 2):
+            title_str = title_str[0:-8]
+        title_str = title_str.strip()
     
-    titolo = soup.findAll('title')
-    print(len(titolo))
-    title_arr = re.split(':',str(titolo[0]))
-    print(title_arr)
-    title_str = title_arr[1]
-    title_str = title_str.strip()
-
     print(title_str)
     for image in soup.findAll('img', alt = title_str):
         if(breaki <= breakPointNumber): #This statement checks the number of images that were restricted to which were supposed to be downloaded
@@ -75,7 +78,7 @@ def perfromScraping(urlReceived, product_id):
                     img = re.sub("[0-9]+_.png$", "600_.png", img)
 
                 filename = nameOfFile
-                folderIndividualName = "AmazonImages/" #Creates the path where the images will be stored
+                folderIndividualName = "AmazonImages2/" #Creates the path where the images will be stored
                 #Create The folder according to search name
                 if not os.path.exists(folderIndividualName):
                     os.makedirs(folderIndividualName)
